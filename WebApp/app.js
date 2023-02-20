ae5667ccd0765be6e4d16089850711b1cfa760f2
NickSavino <nicksavino2@gmail.com>
2023-02-19 20:40:26 -0700

    Basic Web App // Missing most functionality

--- WebApp/app.js
+++ WebApp/app.js
@@ -1,3 +1,4 @@
+
 function toggleDarkMode() {
 
     document.getElementById("dark-button").blur();
@@ -18,8 +19,51 @@ function toggleDarkMode() {
     }
 }
 
-async function main() {
+
+async function loadJson(filename, type) {
+    //Loads Json files for dropdown menus
+    let dropdown = document.getElementById(`${type}-drop`);
+    let dict = await fetchJson(filename);
     
+
+    //populate dropdown menu with values from json file
+    if (type == "region") {
+        for (let code in dict) {
+            let entry = document.createElement("option");
+            entry.textContent=dict[code];
+            entry.value = code
+            dropdown.append(entry);
+        }
+    }
+    else if (type == "series") {
+        for (let code in dict) {
+            let entry = document.createElement("option");
+            entry.textContent=code;
+            entry.value = dict[code]
+            dropdown.append(entry);
+        }
+    }
+}
+
+async function fetchJson(filename) {
+    //fetches local json files
+    let filepath = "./json/" + filename + ".json";
+    let regions = 
+    await fetch(filepath)
+        .then(response => { return response.json();})
+    return regions
+}
+
+function addSeries() {
+    let text_box = document.getElementById("series-selection");
+    let region = document.getElementById("region-drop");
+    let series = document.getElementById("series-drop");
+    text_box.textContent = region.value + " * " + series.value;
+}
+
+async function main() {
+    loadJson("regions_data", "region"); //load regions filter
+    loadJson("series_data", "series"); //load series filter
 }
 
 main();
---
+++ main.py
@@ -0,0 +1,8 @@
+from PullSeries import load_jsons
+
+
+def main():
+    load_jsons()
+    
+if __name__ == '__main__':
+    main()
