80452603076f3af1e96b5cc6d0f74b0cbc588d05
desobob11 <desmond.obrien@ucalgary.ca>
2023-02-19 19:44:42 -0700

    data storing is good, now need data reading

--- Database.py
+++ Database.py
@@ -5,29 +5,40 @@ import sqlalchemy as sql
 from PullSeries import load_jsons
 from SeriesFormatter import *
 
-#pd.DataFrame(final_df).to_sql(con=engine, schema="AESO", name="aeso_csd_t", index=False, if_exists="append")
-def update_series_tables(cursor, engine, df_dict: dict) -> None:
-    for i in df_dict:
+
+'''
+    Function that writes queried series to local sqlite3 database
+
+
+'''
+def update_series_tables(con, engine, df_dict: dict) -> None:
+    # loop over each dataframe in dictionary
+    # remember, key in dictionary is series id
+    # so table names in sql database will be series id
+    for id in df_dict:
+
+        # format name or else will throw sql error
+        table_name = str(id).replace(".", "_")
+
+        # try querying the table
+        # if it doesn't exist, catch exception and write table
+        # if it does exit, remove duplicate rows from table
+        # loaded into memory from df_dict and join with
+        # original queried table.
+        # write this table to databse, replacing original
+        # and essentially just adding new columns
         try:
-            cursor.execute("SELECT * from '%s'" % i)
-            original = pd.read_sql_table(i, "sqlite:///gen_data.db")
-            #original = cursor.execute("SELECT * from '%s'" % i)
-            new = df_dict[i]
-            #print(original)
-            new = pd.DataFrame(new).join(original)
-            new.to_sql(con=engine, name=i, index=False, if_exists="replace")
-        except sql3.OperationalError:
-            df = df_dict[i]
-            df.to_sql(con=engine, name=i, index=False)
+            original = pd.read_sql("SELECT * FROM %s" % table_name, con)
 
+            new = df_dict[id]
 
-
-
-
-
-
-
-
+            # drop common columns, join, and write
+            new = new.drop([i for i in new.columns if i in original.columns], axis=1)
+            new = original.join(new)
+            new.to_sql(con=engine, name=table_name, index=False, if_exists="replace")
+        except Exception:
+            # write brand new table to DB
+            df_dict[id].to_sql(con=engine, name=table_name, index=False)
 
 
 
@@ -44,7 +55,7 @@ def main():
 
     sf = SeriesFormatter()
     sample_data = {"AG.LND.EL5M.UR.K2" : ["AFW", "AFR", "AFE, LDC"],
-                   "AG.LND.EL5M.UR.ZS" : ["AFW", "AFR", "LDC"]}
+                   "AG.LND.EL5M.UR.ZS" : ["AFW", "MNA", "LDC", "ECS"]}
 
 
 
@@ -55,7 +66,9 @@ def main():
     data_dict = sf.pull_compile_data(sample_data)
 
 
-    update_series_tables(c, engine, data_dict)
+
+
+    update_series_tables(con, engine, data_dict)
 
 
 if __name__ == "__main__":
