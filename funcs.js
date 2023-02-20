999c5d161657478a3ceb3c30663ef5e95515618c
desobob11 <desmond.obrien@ucalgary.ca>
2023-02-19 21:26:04 -0700

    working with js

---
+++ funcs.js
@@ -0,0 +1,19 @@
+var output_dict = {}
+
+
+function select_series(series, region) {
+    var series_name = series.value
+    var region_name=  region.value
+
+    
+
+
+
+
+
+
+
+
+}
+
+
---
+++ scratch.js
@@ -0,0 +1,12 @@
+var dict = {}
+
+
+l = [1,2,3,4]
+
+l.push(5)
+
+var json_obj = JSON.parse("jsons/series_data.json")
+
+
+
+
